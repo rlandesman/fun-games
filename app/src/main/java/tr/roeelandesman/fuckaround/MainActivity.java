bf654ce969e9c9191b193f2dbd4a157e77e63a7d
Roee Landesman <roee.landesman@gmail.com>
2017-10-20 12:07:50 -0700

    Initial commit!

---
+++ app/src/main/java/tr/roeelandesman/fuckaround/MainActivity.java
@@ -0,0 +1,19 @@
+package tr.roeelandesman.fuckaround;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginScreen.class);
+        startActivity(intent);
+    }
+
+
+}
